e345ec979e5ae0099803d416fc681c2868a4c89d
tekert-dev <dev@tekert.com.ar>
2023-05-17 03:24:10 -0300

    Bump to 0.9.6-dev

--- lib/src/dp_constants.dart
+++ lib/src/dp_constants.dart
@@ -6,7 +6,7 @@ class DPConst
 {
     // NOTE: The following line of code is modified by tool/grind.dart.
     /// Dart Polisher version number
-    static const String VERSION = "0.9.5";
+    static const String VERSION = "0.9.6-dev";
     // NOTE: The following line of code is modified by tool/grind.dart.
     /// This version is used to embed dart_style as a separate command in cli.
     static const String DART_STYLE_DEP_VERSION = "2.3.1";
